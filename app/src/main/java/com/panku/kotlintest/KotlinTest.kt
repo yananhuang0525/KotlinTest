442e3101f63a6bb6e19efe2a08bc5851207bc7b4
hyn0525 <hyn_0525@sina.com>
2017-06-05 17:22:34 +0800

    s

--- app/src/main/java/com/panku/kotlintest/KotlinTest.kt
+++ app/src/main/java/com/panku/kotlintest/KotlinTest.kt
@@ -1,11 +1,12 @@
 package com.panku.kotlintest
+
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import com.panku.KotlinActivity
 
 /**
  * Date：2017/6/5
@@ -28,13 +29,16 @@ class KotlinTest : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         val id = v.id
-        if (id == R.id.btn) {
-            Toast.makeText(this, et!!.text.toString(), Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, KotlinActivity::class.java))
+//        if (id == R.id.btn) {
+//            Toast.makeText(this, et!!.text.toString(), Toast.LENGTH_LONG).show()
+//            startActivity(Intent(this, KotlinActivity::class.java))
+//        }
+        when (id) {
+            R.id.btn -> startActivity(Intent(this, KotlinActivity::class.java))
         }
     }
 
-    private fun init() {
+    fun init() {
         startActivity(Intent(this, KotlinActivity::class.java))
 
         val intent = Intent()
--- app/src/main/kotlin/com/panku/KotlinActivity.kt
+++ app/src/main/kotlin/com/panku/KotlinActivity.kt
@@ -1,4 +1,4 @@
-package com.panku.kotlintest
+package com.panku
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.panku.kotlintest.R
 
 class KotlinActivity : AppCompatActivity() {
 
